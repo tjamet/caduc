1a5ed80bb97e105c006b4acadfee8a3f5fab74f3
Thibault Jamet <thibault.jamet@gmail.com>
2016-04-02 10:44:21 +0200

    Add debug command line option

--- cddc/cmd.py
+++ cddc/cmd.py
@@ -13,8 +13,6 @@ from cddc.containers import Containers
 from cddc.images import Images
 from cddc.watcher import Watcher
 
-logging.basicConfig(level=logging.INFO)
-
 DEFAULT_DELETE_TIMEOUT = "1d"
 
 def main(argv=sys.argv[1:]):
@@ -23,9 +21,16 @@ def main(argv=sys.argv[1:]):
     parser = OptionParser()
     parser.add_option("--image-gracetime", dest="image_gracetime", default=DEFAULT_DELETE_TIMEOUT,
                       help="Default grace TIME between last container removal (or last child image removal) and proper image removal", metavar="TIME")
+    parser.add_option("-D", '--debug', dest="debug", action='store_true',
+                      help="Switch debug logging on")
     parser.add_option("-c", '--config', dest="config", action='append', default=[],
                       help="Default grace TIME between last container removal (or last child image removal) and proper image removal", metavar="TIME")
     (options, args) = parser.parse_args(argv)
+
+    if options.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
 
     client = docker.Client(**docker.utils.kwargs_from_env(assert_hostname=False))
     config = Config(options.config)
